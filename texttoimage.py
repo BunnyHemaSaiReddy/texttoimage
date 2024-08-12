efe65e9c7387787346fc1ad41d7a0df9ea36e02f
Hema Sai Reddy <158816242+BunnyHemaSaiReddy@users.noreply.github.com>
2024-08-12 19:14:06 +0530

    Update texttoimage.py

--- texttoimage.py
+++ texttoimage.py
@@ -9,7 +9,7 @@ import datetime
 
 def generate_image(prompt, width=512, height=512, number_of_images=3,cfg_scale=10, seed=10):
   for _ in '1':
-      if st.button(":[red]cancel"):
+      if st.button("cancel"):
         break
       os.environ['AWS_ACCESS_KEY_ID'] = bunny_key.access_id   
       os.environ['AWS_SECRET_ACCESS_KEY'] = bunny_key.api_key
@@ -61,11 +61,11 @@ def generate_image(prompt, width=512, height=512, number_of_images=3,cfg_scale=1
 if __name__=="__main__":
     st.markdown(f'<span style="font-size: 25px;">🕗</span> {datetime.datetime.today().hour}:{datetime.datetime.today().minute}:{datetime.datetime.today().second}', unsafe_allow_html=True)
     with st.sidebar :
-      size=st.selectbox("** Enter the size image :",['256x256','512x512','1024x1024','2048x2048'],index=2)
-      number_images=st.slider("** Select the number of images :",min_value=1,max_value=5)
-    prompt=st.text_input('** :blue[Enter a prompt]')
+      size=st.selectbox("Enter the size image :",['256x256','512x512','1024x1024'],index=2)
+      number_images=st.slider("Select the number of images :",min_value=1,max_value=5)
+    prompt=st.text_input('Enter a prompt :')
     if size:
         width=size.split('x')[0]
         height=size.split('x')[1]
-    if st.button("** :black[Generate]"):
+    if st.button("Generate"):
         generate_image(prompt=prompt,width=int(width),height=int(height),number_of_images=int(number_images))
