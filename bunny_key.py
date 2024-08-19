7c68c3c7812768e15b770f2e4e7a3b63a0bebaab
Hema Sai Reddy <158816242+BunnyHemaSaiReddy@users.noreply.github.com>
2024-08-19 14:45:48 +0530

    Update bunny_key.py

--- bunny_key.py
+++ bunny_key.py
@@ -1,12 +1,13 @@
 import mysql.connector
 config = {
     'user': 'avnadmin',
-    'password': 'AVNS_dw-GhEn5H2OmZl82p_a',  
+    'password': 'AVNS_Jw69kwe1u00p9FJrgAH',
     'host': 'mysql-38e489bf-bunnyhemasaireddy-8196.b.aivencloud.com',
     'port': 13610,
-    'database': 'secret_keys'
+    'database': 'defaultdb', 
+    'auth_plugin': 'mysql_native_password'
 }
-
+con = mysql.connector.connect(**config)
 cor=con.cursor()
 cor.execute("select * from secret;")
 r=cor.fetchone()
